b4c777ff94598e0e618f5eeb88aea21363338fef
sami-haq99 <sami.haq99@gmail.com>
2025-09-02 19:08:46 +0100

    gemma pc

--- run-gemma.sh
+++ run-gemma.sh
@@ -20,8 +20,10 @@ source /home/shaq/image-caption/env-ic/bin/activate
 
 image_name_file="multi30k-2016/image_test_2016_flickr.txt"
 output_file="multi30k-2016/"
-language="de"
+language=("fr" "cs")  # Options: de, fr, cs, fi, ro, zh
+
 image_dir="multi30k-2016/images/"
 
-python ic-gemma3b.py $image_name_file $output_file $language $image_dir
-
+for lang in "${language[@]}"; do
+    python ic-gemma3b.py $image_name_file $output_file $lang $image_dir
+done
