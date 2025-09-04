132dce519f20650dc4ac090cb8ce937e559cd1d8
sami-haq99 <sami.haq99@gmail.com>
2025-09-04 16:58:28 +0100

    gema trans

---
+++ run-gemma-trans.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#SBATCH --gres=gpu:rtxa6000:1
+#SBATCH --nodelist=g122
+#SBATCH -p compute
+#SBATCH -J captions
+#SBATCH -t 23:59:59
+#SBATCH -o comet-%j.out
+#SBATCH --mail-type=ALL --mail-user=sami.haq@adaptcentre.ie
+
+
+# For ADAPT Cluster
+
+source /home/shaq/image-caption/env-ic/bin/activate
+
+root_dir="multi30k-2016/"
+image_name_file="${root_dir}image_test_2016_flickr.txt"
+output_file="${root_dir}"
+language=("cs")  # Options: de, fr, cs, fi, ro, zh
+source_file="${root_dir}test_2016_flickr.cs"
+image_dir="${root_dir}images/"
+
+for lang in "${language[@]}"; do
+    python gemma-3-12b-trans.py $image_name_file $output_file $source_file $lang $image_dir
+done
