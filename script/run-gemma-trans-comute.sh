bc7ab8f9c1e347dc74f774f71956e80c4bf98d9d
shaq <shaq@g106.grove.adaptcentre.ie>
2025-09-08 16:31:05 +0100

    comit

--- script/run-gemma-trans-comute.sh
+++ script/run-gemma-trans-comute.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 #SBATCH --gres=gpu:rtxa6000:1
-#SBATCH --nodelist=g122
 #SBATCH -p compute
 #SBATCH -J captions
 #SBATCH -t 23:59:59
@@ -13,7 +12,7 @@
 
 source /home/shaq/image-caption/env-ic/bin/activate
 
-root_dir="COMUTE/"
+root_dir="../COMUTE/"
 
 language=("cs" "de" "fr")  # Options: de, fr, cs, fi, ro, zh
 
@@ -21,7 +20,7 @@ image_dir="${root_dir}images/"
 
 for lang in "${language[@]}"; do
     source_file="${root_dir}en-${lang}/src.en"
-    image_name_file="${root_dir}en-${lang}/image.order"
+    image_name_file="${root_dir}en-${lang}/img.order"
     output_file="${root_dir}en-${lang}/"
     
     python gemma-3-12b-trans.py $image_name_file $output_file $lang $image_dir $source_file
